16eca0662de16b529731542f03283d16341bac93
Sujitha <sujithasankar17@gmail.com>
2024-12-03 19:50:45 +0530

    class,object

---
+++ inheritance.py
@@ -0,0 +1,63 @@
+#single inheritance
+# class parent():
+#     def expenses(self):
+#         print("dad gives expenses")
+# class child(parent):
+#     def needs(self):
+#         print("child has needs")
+# obj=child()
+# obj.needs()
+# obj.expenses()
+#multipleinheritance
+# class dad():
+#     def expenses(self):
+#         print("dad gives the expenses")
+# class mom():
+#     def food(self):
+#         print("mom gives the food")
+# class child(mom,dad):
+#     def needs(self):
+#         print("child has the needs")
+# obj=child()
+# obj.expenses()
+# obj.food()
+# obj.needs()
+#multilevelinheritance
+# class grandpa():
+#     def treasure(self):
+#         print("grandpa gives treasure")
+# class father(grandpa):
+#     def expenses(self):
+#         print("dad gives the expenses")
+# class child(grandpa,father):
+#     def needs(self):
+#         print("child has the needs")
+# obj=child()
+# obj.treasure()
+#heirarchicalinhertance
+# class father():
+#      def expenses(self):
+#          print("father gives the expenses")
+# class child1():
+#      def needs(self):
+#          print("child has the expenses")
+# class child2():
+#          lap="i have laptop"
+#      def phone(self):
+#          print("i have a phone")
+# child=child2()   
+#hybridinheritance
+# class grandpa():
+#     def treasure(self):
+#         print("grandpa gives the treasure")
+# class parent():
+#     def expenses(self):
+#         print("parent gives expenses")
+# class child1(parent,grandpa):
+#     def needs():
+#         print("child needs expenses")
+# class child2(parent,grandpa):
+#     def education():
+#         print("child needs education")
+# obj=child1()
+# obj.treasure
